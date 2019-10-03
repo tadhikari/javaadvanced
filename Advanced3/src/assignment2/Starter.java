9678fb67c3e0035ccddaaf5fa272b1c8ab5d5dc6
Tribikram Adhikari <tribikram@sting.graceland.edu>
2019-10-03 15:27:32 -0500

    intial commit

---
+++ Advanced3/src/assignment2/Starter.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CustomerTester customerTester = new CustomerTester();
+		customerTester.customerMenu();
+
+	}
+
+}
